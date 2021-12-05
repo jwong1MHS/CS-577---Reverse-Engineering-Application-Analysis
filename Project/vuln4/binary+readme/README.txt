d6f9417bd05c7be3c17c429f2709bcbe39d79d2a
jwong1MHS <jasonw5172001@gmail.com>
2021-12-05 01:54:14 -0500

    Added vuln4

---
+++ Project/vuln4/binary+readme/README.txt
@@ -0,0 +1,12 @@
+Run the program in the directory with `./binary3`.
+The program will ask for two total inputs: a first
+name string and an id number.
+
+Goal of the red team:
+There are vulnerabilities in the program that can be triggered.
+The way that you know you found one of the vulnerabilities
+is if a string is printed on the console that says
+"Yikes! You found the ___ vulnerability!", where the ___
+is the nth vulnerability of that program.
+There is one vulnerability that doesn't have a string printed
+however, and the program will abort with an error.
--- Project/vuln4/poc+sourcecode/binary4.c
+++ Project/vuln4/poc+sourcecode/binary4.c
@@ -2,32 +2,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #define NAME_SIZE 100
 #define ID_SIZE 1000
+#define ROT 13
+#define LETTERS 26
 
-char *rot13(const char *src) {
+int randomtime() {
+    int totalseconds = 0;
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    totalseconds = 3600*timeinfo->tm_hour + 60*timeinfo->tm_min + timeinfo->tm_sec;
+    // printf("Total seconds: %d\n", totalseconds);
+    
+    return totalseconds % LETTERS;
+}
+
+char *caesar(const char *src, int shift) {
     if (src == NULL) {
         return NULL;
     }
     char* result = malloc(strlen(src));
+    // static char result[10] = {0};
     if (result != NULL) {
         strcpy(result, src);
+        // printf("Result: %s\n", result);
+        // printf("Shift %d\n", shift);
+
         char* current_char = result;
         while (*current_char != '\0') {
             // Only increment alphabet characters
             if ((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90)) {
-                if (*current_char > 109 || (*current_char > 77 && *current_char < 91)) {
-                    // Characters that wrap around to the start of the alphabet
-                    *current_char -= 13;
+                if (*current_char >= 97 && *current_char <= 122) {
+                    if (shift > (122-*current_char)) {
+                        *current_char = 97+shift-(122-*current_char+1);
+                        // printf("Result: %s\n", result);
+                    } else {
+                        *current_char += shift;
+                        // printf("Result: %s\n", result);
+                    }
                 }
-                else {
-                    // Characters that can be safely incremented
-                    *current_char += 13;
+                if (*current_char >= 65 && *current_char <= 90) {
+                    if (shift > (90-*current_char)) {
+                        *current_char = 65+shift-(90-*current_char+1);
+                        // printf("Result: %s\n", result);
+                    } else {
+                        *current_char += shift;
+                        // printf("Result: %s\n", result);
+                    }
                 }
             }
             current_char++;
         }
     }
+    // printf("Result: %s\n", result);
     return result;
 }
 
@@ -87,7 +118,7 @@ int id_change(int id) {
     }
 }
 
-void mystery_function(char *name, int id) {
+void mystery_function(char *name, int id, int total) {
     int status;
     // printf("%d\n", status);
     puts("What would you like to change about your status?\n1) Name\n2) ID\n3) Neither\n4) Print name and id\n5) Quit");
@@ -97,53 +128,73 @@ void mystery_function(char *name, int id) {
         char *name_new = malloc(10*NAME_SIZE*sizeof(char));
         name_new = name_change(name_new);
         // printf("Name: %s\nID: %d\n", name_new, id);
-        mystery_function(name_new, id);
+        mystery_function(name_new, id, 0);
     }
     else if (status == 2) {
         // change id
-        int id_new = 0;
-        id_new = id_change(id_new);
-        mystery_function(name, id_new);
+        while (true) {
+            char randomID[10] = {0};
+            puts("Random? Type YES for a random ID, otherwise type anything to continue and set your own ID");
+            scanf("%s", randomID);
+            if (strcmp(randomID, "YES") == 0) {
+                // use-after-free
+                id = (int)name[0];
+                if (id == 0) {
+                    puts("You've found a vulnerability!");
+                    exit(0);
+                }
+                break;
+            } else {
+                int id_new = 0;
+                id_new = id_change(id_new);
+                mystery_function(name, id_new, 0);
+                break;
+            }
+        }
     }
     else if (status == 3) {
-        // can double free  
+        // free
         free(name);
-        mystery_function(name, id);
+        mystery_function(name, id, 0);
     }
     else if (status == 4) {
         // print name
         printf("Name: %s\nID: %d\n", name, id);
-        mystery_function(name, id);
+        mystery_function(name, id, 0);
     }
     else if (status == 5) {
         // quit
-        if (strcmp(name,"") == 0) {
-            // Use-after-free
-            puts("You've found a vulnerability!");   
-        }
+        // printf("ID: %d\n", id);
         char *name = malloc(10*NAME_SIZE*sizeof(char));
         puts("Quitting...");
     }
+    else if (status == 6) {
+        total += id;
+        if (total > id) {
+            // Don't touch total
+            puts("You've found a vulnerability!");
+        }
+        mystery_function(name, id, 0);
+    }
     else {
-        mystery_function(name, id);
+        mystery_function(name, id, 0);
     }
 }
 
 void confirm_choice(char *name, int id) {
-    char flag[10] = "flagtext";
+    // char flag[10] = "flagtext";
     char choice[10] = {0};
     puts("Type CONFIRM to confirm your choices:");
     scanf("%s", choice);
     // printf("Flag: %s\n", flag);
-    if (strcmp(rot13(flag), "CHOICE") == 0) { //PUBVPR
-        // Another buffer overflow
-        puts("You've found a vulnerability!");
-    }
-    else if (strcmp(choice, rot13("CHANGE")) == 0) {  //encrypt it
+    // printf("Choice: %s\n",  caesar("CONVERT", randomtime()));
+    // if (strcmp(choice, caesar("CHANGE", 13)) == 0) {
+    if (strcmp(choice, caesar("CHANGE", randomtime())) == 0) {  //encrypt it
         memset(choice, 0, 10);
         puts("Good news! I'm increasing the max size of the name and ID by an order of magnitude.");
-        mystery_function(name, id);
-    }
+        int total;
+        mystery_function(name, id, total);
+    }   
     else if (strcmp(choice, "CONFIRM") != 0) {  //remove stack smashing
         confirm_choice(name, id);
         exit(0);
@@ -151,6 +202,14 @@ void confirm_choice(char *name, int id) {
 }
 
 int main() {
+
+    // for (int i = 0; i < 26; i++) {
+    //     printf("Caesar[%d]: %s\n", i, caesar("abcdefghijklmnopqrstuvwxyz", i));
+    // }
+
+    // for (int i = 0; i < 26; i++) {
+    //     printf("Caesar[%d]: %s\n", i+26, caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ", i));
+    // }
 
     char *name = malloc(10*NAME_SIZE*sizeof(char));
     int id;
